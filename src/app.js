0341c4b191d4c430474d159e3ac1f4a98bd1b97f
unknown <shivamayank001.com>
2020-06-30 00:54:46 +0530

    Add Sense added

--- src/app.js
+++ src/app.js
@@ -165,15 +165,10 @@ app.get('/mail',(req, res)=>{
         subject: sender,
         text: textBody
       };
-      console.log(mailOptions);
-
-
-
       transporter.sendMail(mailOptions, function(error, info){
         if (error) {
           console.log(error);
         } else {
-          console.log('Email sent: ' + info.response);
           res.send(info);
         }
       });
