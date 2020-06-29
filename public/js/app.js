581065361f2e596628589698de0eff035fded2c7
unknown <shivamayank001.com>
2020-06-29 18:47:01 +0530

    Weather Banner Added

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,14 @@
 console.log('This is client side java script file');
 
-
 const weatherForm = document.querySelector('form');
 const searchAddress = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
+const weatherBanner = document.querySelector('.weather-banner');
+const temperature = document.querySelector('#temperature');
+const area = document.querySelector('#location');
+const weatherCondition = document.querySelector('#weather-condition');
+weatherBanner.style.display = 'none';
 var weatherInfo = '';
 
 
@@ -30,14 +34,22 @@ weatherForm.addEventListener('submit',(event)=>{
                     weatherInfo = 'Unable to find weather of given address. Try another search';
                     messageOne.textContent = weatherInfo;
                     messageTwo.textContent = '';
+                    weatherBanner.style.display = 'none';
                 }
                 else{
                     console.log(data.Weather.location);
                     console.log(data.Weather.current);
+                    console.log(data);
                     console.log('Weather outside is '+data.Weather.current.weather_descriptions[0]+'. And temperature is '+data.Weather.current.temperature+ ' Degree Celcius. Humididty is '+data.Weather.current.humidity+'. And precipitation is '+data.Weather.current.precip);
                     weatherInfo = 'Weather outside is '+data.Weather.current.weather_descriptions[0]+'. And temperature is '+data.Weather.current.temperature+ ' Degree Celcius. Humidity is '+data.Weather.current.humidity+'. And precipitation is '+data.Weather.current.precip+'.';
                     messageOne.textContent = data.Weather.location.region + ', '+data.Weather.location.country;
                     messageTwo.textContent = weatherInfo;
+
+                    temperature.textContent = data.Weather.current.temperature + '°';
+                    area.textContent =data.Weather.location.region + ', '+data.Weather.location.country;
+                    weatherCondition.textContent = data.Weather.current.weather_descriptions[0];
+                    weatherBanner.style.display = 'block';
+                    
                 }
                 
             });
